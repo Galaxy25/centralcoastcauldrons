07ca2a0bf54ac2853dbf0d0252b12402c0377c19
Nathan St. John <natestjohn05@gmail.com>
2026-05-18 11:29:55 -0700

    Buy ml capacity only

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -57,9 +57,9 @@ def get_capacity_plan():
         if max_cu <= 0:
             return CapacityPlan(potion_capacity=0, ml_capacity=0)
         capacity = get_capacity(connection)
-    potion_capacity = min(10 - capacity.potion_capacity, math.floor(max_cu / 2))
-    ml_capacity = min(10 - capacity.barrel_capacity, math.ceil(max_cu / 2))
-    return CapacityPlan(potion_capacity=potion_capacity, ml_capacity=ml_capacity)
+    #potion_capacity = min(10 - capacity.potion_capacity, math.floor(max_cu / 2))
+    ml_capacity = min(10 - capacity.barrel_capacity, max_cu)
+    return CapacityPlan(potion_capacity=0, ml_capacity=ml_capacity)
 
 
 @router.post("/deliver/{order_id}", status_code=status.HTTP_204_NO_CONTENT)
